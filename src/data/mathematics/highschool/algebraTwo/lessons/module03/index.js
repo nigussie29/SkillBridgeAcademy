b5c59a937840e675ffdfe21ab1706acbe9c0dfc5
nigussie29 <55414080+nigussie29@users.noreply.github.com>
2026-09-20 00:09:50 -0400

    Register Algebra II discriminant lesson

--- src/data/mathematics/highschool/algebraTwo/lessons/module03/index.js
+++ src/data/mathematics/highschool/algebraTwo/lessons/module03/index.js
@@ -1,11 +1,13 @@
 import lesson01 from "./lesson01.js";
 import lesson02 from "./lesson02.js";
 import lesson03 from "./lesson03.js";
+import lesson04 from "./lesson04.js";
 
 const module03Lessons = [
   lesson01,
   lesson02,
   lesson03,
+  lesson04,
 ];
 
 export default module03Lessons;
