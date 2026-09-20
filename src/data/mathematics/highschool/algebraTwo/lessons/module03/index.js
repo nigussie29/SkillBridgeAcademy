39b00e9e3bbe42c59ee3a9e6b4ad3bfdca36929f
nigussie29 <55414080+nigussie29@users.noreply.github.com>
2026-09-19 23:46:01 -0400

    Register Algebra II quadratic formula lesson

--- src/data/mathematics/highschool/algebraTwo/lessons/module03/index.js
+++ src/data/mathematics/highschool/algebraTwo/lessons/module03/index.js
@@ -1,9 +1,11 @@
 import lesson01 from "./lesson01.js";
 import lesson02 from "./lesson02.js";
+import lesson03 from "./lesson03.js";
 
 const module03Lessons = [
   lesson01,
   lesson02,
+  lesson03,
 ];
 
 export default module03Lessons;
