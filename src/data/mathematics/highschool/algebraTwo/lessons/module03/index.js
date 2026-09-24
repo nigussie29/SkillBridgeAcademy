e365e4c64bded123e0886825055191805e0a212a
nigussie29 <55414080+nigussie29@users.noreply.github.com>
2026-09-24 04:44:37 -0400

    Register A2-M3-L5 Nature of the Roots

--- src/data/mathematics/highschool/algebraTwo/lessons/module03/index.js
+++ src/data/mathematics/highschool/algebraTwo/lessons/module03/index.js
@@ -2,12 +2,14 @@ import lesson01 from "./lesson01.js";
 import lesson02 from "./lesson02.js";
 import lesson03 from "./lesson03.js";
 import lesson04 from "./lesson04.js";
+import lesson05 from "./lesson05.js";
 
 const module03Lessons = [
   lesson01,
   lesson02,
   lesson03,
   lesson04,
+  lesson05,
 ];
 
 export default module03Lessons;
